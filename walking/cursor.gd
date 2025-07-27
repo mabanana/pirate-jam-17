1ab0c9ab11e5b584025b6a0fe0afa75bceff2fa1
mabanana <aldwin.yu@hotmail.com>
2025-07-27 02:45:20 -0400

    Cursor Fix

--- walking/cursor.gd
+++ walking/cursor.gd
@@ -5,7 +5,6 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

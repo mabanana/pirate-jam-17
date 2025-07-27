786ba653ed7b522c367aa7221588203397d81f6d
mabanana <aldwin.yu@hotmail.com>
2025-07-27 01:19:40 -0400

    visual adjustments and computer

--- Room/map.gd
+++ Room/map.gd
@@ -1,14 +1,18 @@
 extends Node3D
 
 @export var floor_plane: MeshInstance3D
-@export var floor_size = Vector2(10, 8)
+@export var floor_size = Vector2(6, 8)
 var cell_size = 2.0
 var margin = 1
 var grid_map = {}
-var grid_pos = [2,0]
+var grid_pos = [1,2]
 
 var player_move_requested = Signals.player_move_requested
 var player_move_resolved = Signals.player_move_resolved
+
+var object_map = {
+	"mattress": [0, 2],
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,7 +23,9 @@ func _ready():
 		for col in range(cols):
 			var top_left = position - Vector3(floor_size.x / 2, position.y, floor_size.y / 2)
 			grid_map[[row, col]] = top_left + Vector3( margin*cell_size*(row+margin), 0, margin*cell_size*(col+margin))
-	
+	for object_grid_pos in object_map.values():
+		if object_grid_pos in grid_map:
+			grid_map.erase(object_grid_pos)
 	player_move_requested.connect(_move_player)
 	$"../Player".position = grid_map[grid_pos]
 
